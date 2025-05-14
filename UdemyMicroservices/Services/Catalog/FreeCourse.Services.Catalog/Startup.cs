92d83eeb02b7a8e489d0e7e00c391ef45f02d52b
ozanbelkeci <belkeci.ozan@gmail.com>
2025-05-15 00:46:16 +0300

    Dto Nesneler
    Dto nesnelerin bir kısmı oluşturuldu.

--- UdemyMicroservices/Services/Catalog/FreeCourse.Services.Catalog/Startup.cs
+++ UdemyMicroservices/Services/Catalog/FreeCourse.Services.Catalog/Startup.cs
@@ -1,3 +1,4 @@
+using FreeCourse.Services.Catalog.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -5,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
@@ -26,7 +28,15 @@ namespace FreeCourse.Services.Catalog
         public void ConfigureServices(IServiceCollection services)
         {
 
+            services.AddAutoMapper(typeof(Startup));
             services.AddControllers();
+
+            services.Configure<DatabaseSettings>(Configuration.GetSection("DatabaseSettings"));
+            services.AddSingleton<IDatabaseSettings>(sp =>
+            {
+                return sp.GetRequiredService<IOptions<DatabaseSettings>>().Value;
+            });
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "FreeCourse.Services.Catalog", Version = "v1" });
