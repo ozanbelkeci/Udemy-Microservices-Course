026bcbadf186467861b35b9d683fe7feb8e3f9b9
ozanbelkeci <84995180+ozanbelkeci@users.noreply.github.com>
2025-06-12 17:07:29 +0300

    12.06.2025

--- UdemyMicroservices/Services/Catalog/FreeCourse.Services.Catalog/obj/Debug/net5.0/FreeCourse.Services.Catalog.AssemblyInfo.cs
+++ UdemyMicroservices/Services/Catalog/FreeCourse.Services.Catalog/obj/Debug/net5.0/FreeCourse.Services.Catalog.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FreeCourse.Services.Catalog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb2edce3989e0648d8725c13cebf40507c19c1be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc1e1370c015a3c92b1515d1036dbc97b40bff43")]
 [assembly: System.Reflection.AssemblyProductAttribute("FreeCourse.Services.Catalog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FreeCourse.Services.Catalog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
