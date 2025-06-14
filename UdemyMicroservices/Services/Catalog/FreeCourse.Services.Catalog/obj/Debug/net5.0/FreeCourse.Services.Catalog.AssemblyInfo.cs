a2b15186bba90362dc350351714c472c37a38406
ozanbelkeci <belkeci.ozan@gmail.com>
2025-06-14 11:55:56 +0300

    vs add git ignore

--- UdemyMicroservices/Services/Catalog/FreeCourse.Services.Catalog/obj/Debug/net5.0/FreeCourse.Services.Catalog.AssemblyInfo.cs
+++ UdemyMicroservices/Services/Catalog/FreeCourse.Services.Catalog/obj/Debug/net5.0/FreeCourse.Services.Catalog.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FreeCourse.Services.Catalog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+026bcbadf186467861b35b9d683fe7feb8e3f9b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f60ec08afd0b20aadd6cc9eccd00f19c99366d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("FreeCourse.Services.Catalog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FreeCourse.Services.Catalog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
